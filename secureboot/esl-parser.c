e4b0795b8bee548344bd44fd194af52253832a57
43313EB9AA87E7039F8F3948282E61C0CB12372C5499884609A01B2BCA37B973 <36488804+iadgovuser34@users.noreply.github.com>
2021-02-10 22:40:06 -0500

    Update esl-parser.c
    Upgraded parser. Needed a way to parse db and dbx backups produced by Windows PowerShell. Need to look into supporting more EFI_GUID values beyond EFI_CERT_X509_GUID and EFI_CERT_SHA256_GUID.

--- secureboot/esl-parser.c
+++ secureboot/esl-parser.c
@@ -2,188 +2,323 @@
 *  US Government source code released to the public domain
 *  A product of NSA's Cybersecurity Directorate
 *
-*  Lol, yeah, this code is hacked together quickly and could be so much more optimized. FIXME
+*  Parse ESL (EFI Signature List) files produced by Linux or Windows utilities.
+*    Complex files with multiple sequential lists and numerous records are
+*    supported. Mixed type ESLs are also supported.
+*
+*  Input: An ESL file is expected for parsing. ESL files can be created by
+*    Linux efi-tools utilities or Windows PowerShell. Multiple lists can be in
+*    a single ESL file. ESLs containing certificates and hashes are supported.
+*    The .esl extension is not required. ESL files are expected to be binary
+*    data.
+*
+*  Output: EFI certificates are DER encoded. Each unique certificate detected
+*    will be written to a file outfilename.###.cer. The #'s are replaced by an
+*    integer that starts at 001 and increments up to 999. Files with matching
+*    names will be overwritten.
+*
+*    Hashes are appended to the file outfilename.txt. Each line consists of an
+*    originator UUID and SHA-256 hash. Both values are in hex. Outfilename.txt
+*    will always be appended to -- not overwritten or cleared/deleted.
+*
+*  Code supports Linux and Windows.
+*  Linux compile: gcc esl-parser.c -o esl-parser -std=c99
+*  Windows compile: cl esl-parser.c
 */
-
-#define _CRT_SECURE_NO_DEPRECATE
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <limits.h>
 
+//constants
+#define PROGRAM_VERSION 1
+#define MAX_FILENAME_LENGTH 1024
+
+//error codes
+#define ERROR_HELP 1
+#define ERROR_VERSION 2
+#define ERROR_INFILE_MISSING 10
+#define ERROR_INFILE_LENGTH 11
+#define ERROR_INFILE_READ 12
+#define ERROR_INFILE_CONTENT 13
+#define ERROR_INFILE_STRUCTURE 14
+#define ERROR_INFILE_TRUNCATION 15
+#define ERROR_OUTFILE_MISSING 20
+#define ERROR_OUTFILE_LENGTH 21
+#define ERROR_OUTFILE_OPEN 22
+
+// **** PRINT HELP INFO **** //
+void printHelp() {
+  printf("Use: esl-parser -i infile.esl -o outfile\n"
+         "  This program parses EFI Signature List (ESL) files produced by Linux\n"
+         "  and Windows utilities. ESL files are expected to be in binary format\n"
+         "  and contain one or more SHA-256 hashes and/or certificates. Mixed\n"
+         "  lists are supported. Multiple appended/concatenated lists are also\n"
+         "  supported\n"
+         "\n"
+         "  Inputs: --in | -i | /i [infile]    Name of binary esl input file\n"
+         "          --out | -o | /o [outfile]  Base name of file to be written to\n"
+         "          --version | -v | /v        Display version number\n"
+         "          --help | -h | /h           Display this help message\n"
+         "          --debug | -d | /d          Print additional debugging details\n"
+         "          --silent | -s | /s         Do not print error messages\n"
+         "\n"
+         "  Outputs: Outfile.txt shall contain a list of detected hashes as:\n"
+         "             [Originator UUID] [SHA-256]\n"
+         "           Hash records/lines are appended to outfile.txt. The file is\n"
+         "           not cleared or overwritten. Outfile.txt will be created if it\n"
+         "           does not exist.\n"
+         "\n"
+         "           Outfile.###.cer shall contain a DER encoded certificate. One\n"
+         "           certificate per file. ### increments from 001 to 999. Files\n"
+         "           will be overwritten if they exist, or created if they do not\n"
+         "           exist.\n"
+         "\n"
+         "  Example usage:\n"
+         "    esl-parser -i db.esl -o frontdeskpc\n"
+         "\n"
+         "    esl-parser -i dbx.old.esl -o exclusions -d\n"
+         "\n"
+         "    esl-parser -v\n");
+  exit(ERROR_HELP);
+}
+
+// **** PRINT VERSION INFO **** //
+void printVersion() {
+  printf("%i\n", PROGRAM_VERSION);
+  exit(ERROR_VERSION);
+}
+
+// **** SHA-256 HASH PARSER **** //
+int parseHashes(bool debug, bool silent, FILE *infile, unsigned int hashcount, unsigned char *outfilename) {
+  unsigned int  listsize = 0,
+                headersize = 0,
+                sigsize = 0,
+                records = 0,
+                readcount = 0;
+  unsigned char *buf = NULL,
+                outfilenameEX[strlen(outfilename + 4)];
+  FILE          *outfile = NULL;
+
+  //read the structure sizing and record count info
+  fread(&listsize, 4, 1, infile);
+  fread(&headersize, 4, 1, infile);
+  fread(&sigsize, 4, 1, infile);
+  records = (listsize - 28) / sigsize;
+
+  if(debug) printf("Parsing hash list\n  listsize %i, header %i, sigsize %i, records %i\n", listsize, headersize, sigsize, records);
+  
+  //add the txt extension to the filename
+  outfilenameEX[0] = '\0';
+  strcat(outfilenameEX, outfilename);
+  strcat(outfilenameEX, ".txt");
+
+  //open the outfile for appending
+  if((outfile = fopen(outfilenameEX, "a")) == NULL) {
+    if(!silent) printf("Error: could not open outfile %s\n", outfilenameEX);
+    return UINT_MAX - 1;
+  }
+
+  if(debug) printf("Writing hashes to %s\n", outfilenameEX);
+
+  //set up a buffer big enough for origiinator UUID and SHA-256
+  buf = calloc(sigsize, sizeof(unsigned char));
+  for(int i = 0; i < records; i++) {
+    //read originator UUID and SHA-256 records one at a time
+    readcount = fread(buf, sigsize, 1, infile);
+    if(debug) printf("  Parsing hash %i (%i bytes, %i read boolean)\n", hashcount, sigsize, readcount);
+    
+    //validate that all 48 bytes were read (16 originator UUID + 32 SHA-256 = 48)
+    if(readcount != 1) {
+      if(!silent) printf("Error: Signature size exceeds bytes actually read.\n");
+      free(buf);
+      fclose(outfile);
+      return UINT_MAX;
+    }
+    
+    //write each byte to file
+    for(int j = 0; j < sigsize; j++) {
+      fprintf(outfile, "%02x", buf[j]);    //write as hex string with a zero pad if necessary
+      
+      //add hashes to the originator UUID, and a space between it and the hash
+      if(j == 4 || j == 6 || j == 8 || j == 10)
+        fprintf(outfile, "-");
+      else if(j == 16)
+        fprintf(outfile, " ");
+    }
+    
+    fprintf(outfile, "\n");
+    hashcount++;
+  }
+  
+  free(buf);
+  fclose(outfile);
+  return hashcount;
+}
+
+// **** CERTIFICATE PARSER **** //
+int parseCerts(bool debug, bool silent, FILE *infile, unsigned int certcount, unsigned char *outfilename) {
+  unsigned int  listsize = 0,
+                headersize = 0,
+                sigsize = 0,
+                records = 0,
+                readcount = 0;
+  unsigned char *buf = NULL,
+                outfilenameEX[MAX_FILENAME_LENGTH + 7],
+                temp[4];
+  FILE          *outfile = NULL;
+  
+  //read size and record count info
+  fread(&listsize, 4, 1, infile);
+  fread(&headersize, 4, 1, infile);
+  fread(&sigsize, 4, 1, infile);
+  records = (listsize - 28) / sigsize;
+
+  if(debug) printf("Parsing cert list\n  listsize %i, header %i, sigsize %i, records %i\n", listsize, headersize, sigsize, records);
+
+  //null terminate the outfile name
+  outfilenameEX[0] = '\0';
+  strcat(outfilenameEX, outfilename);
+  
+  //make a buffer big enough to contaiin each cert
+  buf = calloc(sigsize, sizeof(unsigned char));
+  for(int i = 0; i < records; i++) {
+    //copy the entire originator UUID and cert into our buffer
+    readcount = fread(buf, sigsize, 1, infile);
+    if(debug) printf("  Parsing cert %i (%i bytes, %i read boolean)\n", certcount, sigsize, readcount);
+    
+    //validate that the file had the amount of data the structure indicated
+    if(readcount != 1) {
+      if(!silent) printf("Error: Signature size exceeds bytes actually read.\n");
+      free(buf);
+      return UINT_MAX;
+    }
+    
+    //append the outfilename with a 3-digit number and the cer extension (certs are DER encoded)
+    sprintf(temp, "%03d", certcount);
+    strcat(outfilenameEX, temp);
+    strcat(outfilenameEX, ".cer");
+
+    //open the outfile
+    if((outfile = fopen(outfilenameEX, "w")) == NULL) {
+      if(!silent) printf("Error: could not open outfile %s\n", outfilenameEX);
+      return UINT_MAX - 1;
+    }
+
+    //one certificate per file
+    if(debug) printf("Writing certificate to %s\n", outfilenameEX);
+    
+    //write the certificate to the file, but omit the originator UUID
+    for(int j = 16; j < sigsize; j++) {
+      fputc(buf[j], outfile);
+    }
+    
+    fclose(outfile);
+    certcount++;
+  }
+  
+  free(buf);
+  return certcount++;
+}
+
+// **** MAIN FUNCTION **** //
 int main(int argc, char *argv[]) {
-  float         version = 1.1;
-  FILE          *infile,
-                *outfile;
-  unsigned char *inbuf,
-                *outbuf;
-  unsigned int  inbuflen,
-                inbufcalclen,
-                outbuflen,
-                temp,
-                loops;
+  bool          debug = false,
+                silent = false;
+  char          *infilename = NULL,
+                *outfilename = NULL;
+  unsigned char efisigtype[16];
+  FILE          *infile = NULL;
+  unsigned int  infilelen = 0,
+                hashcount = 1,
+                certcount = 1,
+                offset = 0;
 
-//HELP AND VERSION TEXT OUTPUT CHECKS RETURN 0
-  //display help
-  if(argc > 1 && (strcmp("--help", argv[1]) == 0 || strcmp("-h", argv[1]) == 0)) {
-    printf("Use: esl-parser infile.esl outfile.txt\n"
-           "  This program parses EFI Signature List (ESL) files. Hashes are recorded in\n"
-           "    hexadecimal format in a text (TXT) file. Certificates are stored as\n"
-           "    uuid.cer.\n"
-           "\n"
-           "  Inputs: infile.esl is a text string name for the ESL input file.\n"
-           "          outfile.txt is a text file to be populated with hex values.\n"
-           "  Outputs: A text file full of hashes, or certificates(s).\n"
-           "\n"
-           "  Example usage:\n"
-           "    esl-parser allowlist.esl allowed.txt\n");
-    return 0;
-    }
-
-  //display version
-  if(argc > 1 && (strcmp("--version", argv[1]) == 0 || strcmp("-v", argv[1]) == 0)) {
-    printf("%f\n", version);
-    return 0;
-    }
-
-//ARGUMENT COUNT CHECKS RETURN 1
-  //argument count check
-  if(argc != 3) {
-    printf("Error: exactly 2 arguments (infile, outfile) are required\n");
-    return 1;
-    }
-
-//ARGUMENT VALIDATION RETURN 2
-  //validate the output filename
-  if(strlen(argv[1]) > 32) {
-    printf("Error: infile name too long\n");
-    return 2;
-    }
-  if(strlen(argv[2]) > 32) {
-    printf("Error: outfile name too long\n");
-    return 2;
-    }
-
-  //open the infile
-  if((infile = fopen(argv[1], "r")) == NULL) {
-    printf("Error: could not open infile\n");
-    return 3;
-  }
-  
-  //get the length of the infile
-  fseek(infile, 0, SEEK_END);
-  inbuflen = ftell(infile);
-  fseek(infile, 0, SEEK_SET);
-  
-  //allocate a buffer to contain the infile
-  inbuf = (char*)calloc(inbuflen, sizeof(char));
-  
-  //read the entire infile into the buffer
-  fread(inbuf, sizeof(char), inbuflen, infile);
-  
-  //check to make sure we have at least 28 bytes
-  if(inbuflen < 28) {
-    printf("Error: infile is too short\n");
-    free(inbuf);
-    fclose(infile);
-    return 3;
-  }
-  
-  //read the file length as recorded in the ESL structure
-  inbufcalclen = 0;
-  inbufcalclen = (inbuf[19] << 24) | (inbuf[18] << 16) | (inbuf[17] << 8) | inbuf[16];
-  
-  //our seek size should match the ESL structure's size
-  if(inbuflen != inbufcalclen) {
-    printf("Error: infile length and calculated EFI_GUID structure length mismatched\n");
-    free(inbuf);
-    fclose(infile);
-    return 3;
-  }
-
-//INFILE VALIDATION RETURN 3
-  //Check for hashes
-  if(inbuf[0] == 38 && inbuf[15] == 40) {
-  //HASHES
-    //Infile must be (size - 28) % 48 = 0 when hashes are present
-    if(((inbufcalclen - 28) % 48) != 0) {
-      printf("Error: infile length - 28 must be a multiple of 48\n");
-      free(inbuf);
-      fclose(infile);
-      return 3;
-    }
-    
-    //calculate number of loops
-    loops = (inbufcalclen - 28) / 48;
-    
-    //data in the infile is good enough, so parse it to the outfile
-    outbuflen = inbufcalclen - 28;
-    outbuf = (char*)calloc(outbuflen, sizeof(char));
-    for(int i = 0; i < loops; i++) {
-      //convert mixed-endian Microsoft COM/OLE format to normal big-endian
-      outbuf[i*48+3] = inbuf[i*48+28];
-      outbuf[i*48+2] = inbuf[i*48+1+28];
-      outbuf[i*48+1] = inbuf[i*48+2+28];
-      outbuf[i*48] = inbuf[i*48+3+28];
-      
-      outbuf[i*48+5] = inbuf[i*48+4+28];
-      outbuf[i*48+4] = inbuf[i*48+5+28];
-
-      outbuf[i*48+7] = inbuf[i*48+6+28];
-      outbuf[i*48+6] = inbuf[i*48+7+28];
-      
-      outbuf[i*48+8] = inbuf[i*48+8+28];
-      outbuf[i*48+9] = inbuf[i*48+9+28];
-      outbuf[i*48+10] = inbuf[i*48+10+28];
-      outbuf[i*48+11] = inbuf[i*48+11+28];
-      outbuf[i*48+12] = inbuf[i*48+12+28];
-      outbuf[i*48+13] = inbuf[i*48+13+28];
-      outbuf[i*48+14] = inbuf[i*48+14+28];
-      outbuf[i*48+15] = inbuf[i*48+15+28];
-      
-      //grab the hash
-      for(int j = 16; j < 48; j++)
-        outbuf[i*48+j] = inbuf[i*48+j+28];
-    }
-    
-  //OUTFILE ERRORS RETURN 4
-    //open output file for writing and overwriting
-    if((outfile = fopen(argv[2], "wb+")) == NULL) {
-      printf("Error: could not open outfile\n");
-      free(outbuf);
-      free(inbuf);
-      fclose(infile);
-      return 4;
-    }
-
-    for(int i = 0; i < loops; i++) {
-      //write content to output file
-      for(int j = 0; j < 48; j++) {
-        if(j == 4 || j == 6 || j == 8 || j == 10) fprintf(outfile, "-");
-        else if(j == 16) fprintf(outfile, " ");
-        
-        fprintf(outfile, "%02x", outbuf[i * 48 + j]);
-        
-        if(j == 47) fprintf(outfile, "\n");
-      }
-    }
-  }
-  else if(inbuf[0] == 161 && inbuf[15] == 114) {
-  //CERTIFICATES
-    printf("certificates\n");
-    //data in the infile is good enough, so parse it into outfiles
-    //FIXME
-  }
+  //process arguments
+  if(argc < 2)
+    printHelp();  //always print help if there are not enough arguments
   else {
-    printf("Error: failed to detect EFI_CERT_X509_GUID or EFI_CERT_SHA256_GUID in infile\n");
-    free(inbuf);
+    for(int i = 1; i < argc; i++) {
+      if(!strcmp("-h", argv[i]) || !strcmp("/?", argv[i]) || !strcmp("--help", argv[i]))
+        printHelp();
+      else if(!strcmp("-v", argv[i]) || !strcmp("/v", argv[i]) || !strcmp("--version", argv[i]))
+        printVersion();
+      else if(!strcmp("-d", argv[i]) || !strcmp("/d", argv[i]) || !strcmp("--debug", argv[i]))
+        debug = true;
+      else if(!strcmp("-s", argv[i]) || !strcmp("/s", argv[i]) || !strcmp("--silent", argv[i]))
+        silent = true;
+      else if((!strcmp("-i", argv[i]) || !strcmp("/i", argv[i]) || !strcmp("--in", argv[i])) && (i + 1) < argc)
+        infilename = argv[++i];
+      else if((!strcmp("-o", argv[i]) || !strcmp("/o", argv[i]) || !strcmp("--out", argv[i])) && (i + 1) < argc)
+        outfilename = argv[++i];
+    }
+  }
+  
+  //validate arguments
+  if(infilename == NULL) {
+    if(!silent) printf("Error: Infile is missing.\n");
+    exit(ERROR_INFILE_MISSING);
+  }
+  else if(strlen(infilename) > MAX_FILENAME_LENGTH) {
+    if(!silent) printf("Error: Infile name is too long (max %i).\n", MAX_FILENAME_LENGTH);
+    exit(ERROR_INFILE_LENGTH);
+  }
+    
+  if(outfilename == NULL) {
+    if(!silent) printf("Error: Outfile is missing.\n");
+    exit(ERROR_OUTFILE_MISSING);
+  }
+  else if(strlen(outfilename) > MAX_FILENAME_LENGTH) {
+    if(!silent) printf("Error: Outfile name is too long (max %i).\n", MAX_FILENAME_LENGTH);
+    exit(ERROR_OUTFILE_LENGTH);
+  }
+  
+  //open infile
+  if((infile = fopen(infilename, "r")) == NULL) {
+    if(!silent) printf("Error: Could not open infile.\n");
+    exit(ERROR_INFILE_READ);
+  }
+  
+  //validate infile by length
+  fseek(infile, 0, SEEK_END);
+  infilelen = ftell(infile);
+  fseek(infile, 0, SEEK_SET);
+  if(infilelen < 76) {
+    if(!silent) printf("Error: Infile is too short. Minimum length is 76 bytes.\n");
     fclose(infile);
-    return 3;
+    exit(ERROR_INFILE_CONTENT);
+  }
+  
+  //parse the infile
+  while(ftell(infile) < infilelen) {
+    //read the EFI_GUID
+    fread(efisigtype, 16, 1, infile);
+
+    //branch based off the first and last values of EFI_GUID (validate by values)
+    if(efisigtype[0] == 38 && efisigtype[15] == 40)
+      hashcount = parseHashes(debug, infile, hashcount, outfilename);
+    else if(efisigtype[0] == 161 && efisigtype[15] == 114)
+      certcount = parseCerts(debug, infile, certcount, outfilename);
+    else {
+      if(!silent) printf("Error: Malformed infile header structure.\n");
+      fclose(infile);
+      exit(ERROR_INFILE_STRUCTURE);
+    }
+    
+    //handle parser errors
+    if(hashcount == UINT_MAX || certcount == UINT_MAX) {
+      fclose(infile);
+      exit(ERROR_INFILE_TRUNCATION);
+    }
+    else if(hashcount == UINT_MAX - 1 || certcount == UINT_MAX - 1) {
+      fclose(infile);
+      exit(ERROR_OUTFILE_OPEN);
+    }
   }
 
-  free(outbuf);  
-  free(inbuf);
+  //cleanup
   fclose(infile);
-
   return 0;
 }
